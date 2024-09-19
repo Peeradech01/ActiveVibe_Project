b69c9a96ce58638390f1c9e6604dcbefc3041a34
Peeradech01 <suapeeradech@gmail.com>
2024-09-19 14:07:02 +0700

    edit form radio register

--- Projectors/fitnessmanagement/fitness/forms.py
+++ Projectors/fitnessmanagement/fitness/forms.py
@@ -7,9 +7,17 @@ from django.contrib.auth.forms import UserCreationForm
 class LoginForm(forms.Form):
     username = forms.CharField(max_length=65)
     password = forms.CharField(max_length=65, widget=forms.PasswordInput)
+    
 
 
 class RegisterForm(UserCreationForm):
+    ROLE_CHOICES = [
+        ('Customer', 'Customer'),
+        ('Trainer', 'Trainer'),
+    ]
+    
+    role = forms.ChoiceField(choices=ROLE_CHOICES, widget=forms.RadioSelect, label='Role')
+    
     class Meta:
         model=User
         fields = ['first_name', 'last_name', 'username', 'email', 'password1', 'password2'] 
