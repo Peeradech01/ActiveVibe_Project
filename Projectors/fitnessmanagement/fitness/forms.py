e112f19d4645445a2049bd934e82dbe5bd6a602d
Peeradech01 <suapeeradech@gmail.com>
2024-09-24 23:24:36 +0700

    edit form login

--- Projectors/fitnessmanagement/fitness/forms.py
+++ Projectors/fitnessmanagement/fitness/forms.py
@@ -1,9 +1,9 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm, AuthenticationForm
 
-class LoginForm(forms.Form):
+class LoginForm(AuthenticationForm):
     username = forms.CharField(max_length=65)
     password = forms.CharField(max_length=65, widget=forms.PasswordInput)
     
