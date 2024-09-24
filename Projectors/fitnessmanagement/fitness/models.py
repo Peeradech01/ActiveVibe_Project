69c35480f33e808f598687ca66986f70410e909a
Peeradech01 <suapeeradech@gmail.com>
2024-09-24 20:22:53 +0700

    Add Membership models

--- Projectors/fitnessmanagement/fitness/models.py
+++ Projectors/fitnessmanagement/fitness/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Membership(models.Model):
+    name = models.CharField(max_length=50, null=False)
+    description = models.CharField(max_length=100, null=False)
+    price = models.FloatField(null=False)
+    duration = models.IntegerField(null=False)
+
+    def __str__(self):
+        return self.name
+    
