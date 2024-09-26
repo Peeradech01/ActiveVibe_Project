4a4bc15cc7daa8a9fdb1c101ca92cf9f31f45100
65070129 <65070129@kmitl.ac.th>
2024-09-26 13:32:22 +0700

    commit

--- Projectors/fitnessmanagement/fitness/models.py
+++ Projectors/fitnessmanagement/fitness/models.py
@@ -24,16 +24,6 @@ class PersonalInfo(models.Model):
     bmi = models.FloatField(null=False, default=0)
     phone = models.CharField(max_length=10, null=False)
 
-class Schedules(models.Model):
-    STATUS_CHOICES = [
-        ('active', 'Active'),
-        ('completed', 'Completed'),
-    ]
-
-    start_time = models.DateTimeField(null=False, blank=False)
-    end_time = models.DateTimeField(null=False, blank=False)
-    status = models.CharField(max_length=10, choices=STATUS_CHOICES, null=False, blank=False)
-
 
 class Category(models.Model):
     name = models.CharField(max_length=50, null=False)
@@ -46,10 +36,19 @@ class FitnessClass(models.Model):
     user = models.ManyToManyField(User)
     name = models.CharField(max_length=50, null=False)
     description = models.TextField(null=False)
-    schedule = models.ForeignKey(Schedules, on_delete=models.CASCADE)
-    categories = models.ForeignKey(Category, on_delete=models.CASCADE) 
+    categories = models.ForeignKey(Category, on_delete=models.CASCADE, default=None) 
     trainer = models.ForeignKey(Group, on_delete=models.CASCADE, null=False)
     max_capacity = models.IntegerField(null=False) 
 
     def __str__(self):
         return self.name
+
+class Schedules(models.Model):
+    STATUS_CHOICES = [
+        ('active', 'Active'),
+        ('completed', 'Completed'),
+    ]
+    fitnessclass = models.ForeignKey(FitnessClass, on_delete=models.CASCADE, default=None)
+    start_time = models.DateTimeField(null=False, blank=False)
+    end_time = models.DateTimeField(null=False, blank=False)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, null=False, blank=False)
